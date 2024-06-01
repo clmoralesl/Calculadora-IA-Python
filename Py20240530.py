08680881bbceb46a9725e111d958bce86a149e04
clmoralesl <cl.moralesl@duocuc.cl>
2024-06-01 00:41:04 -0400

    20240530(Diccionarios-Funciones)(Calculadora IA).py

--- Py20240530.py
+++ Py20240530.py
@@ -1,60 +1,46 @@
 print ('Bienvenido a calculadora IA')
 diccionariocalculos = {}
-def ingresodatos(llave,valor):
-    diccionariocalculos[llave] = valor
-def borrardatos(llave):
-    if llave in diccionariocalculos:
-        del diccionariocalculos[llave]
-        print(f'Operación {llave} borrada exitosamente')
-    else:
-        print(f'No se ha encontrado la operación {llave}')
-def probarllave(llave):
-    for llaveprueba, valor in diccionariocalculos.items():
-        if llaveprueba == llave:
-            print(valor)
-while True:
-    try:
-        print ('''
------------Calculadora-----------
-1) Ingresar datos al diccionario
-2) Borrar datos del diccionario
-3) Listar los datos del diccionario
-4) Probar la operación
-5) Salir del programa
-    ''')
-        opcionmenu = int(input('Ingrese una opción:\n'))
-    except ValueError:
-        print ('Error de datos, intente nuevamente')
-    if opcionmenu == 1:
-            while True:
-                print('-----------Ingreso de datos-----------')
-                llaveingreso = input('Ingrese el nombre de la operatoria:\n').capitalize()
-                valoringreso = input('Ingrese el cálculo de la operatoria:\n')
-                ingresodatos(llaveingreso,valoringreso)
-                print(diccionariocalculos)
-                while True:
-                    try:
-                        print('¿Desea ingresar otra operación?')
-                        print('1) SI')
-                        print('2) NO')
-                        continuaringreso = int(input())
-                        if continuaringreso !=1 and continuaringreso !=2:
-                            print('Ingrese una opción válida')
-                            continue
-                        else:
-                            break
-                    except ValueError:
-                        print('Error de dato, vuelva a intentar')
-                        continue
-                if continuaringreso == 1:
+def ingresodatos():
+    while True:
+        print('-----------Ingreso de datos-----------')
+        llaveingreso = input('Ingrese el nombre de la operatoria:\n').capitalize()
+        valoringreso = input('Ingrese el cálculo de la operatoria:\n')
+        diccionariocalculos[llaveingreso] = valoringreso
+        print('Se ha ingresado correctamente la siguiente opertaria:')
+        print(f'{llaveingreso} = {valoringreso}')
+        while True:
+            try:
+                print('¿Desea ingresar otra operación?')
+                print('1) SI')
+                print('2) NO')
+                continuaringreso = int(input())
+                if continuaringreso !=1 and continuaringreso !=2:
+                    print('Ingrese una opción válida')
                     continue
                 else:
                     break
-    elif opcionmenu == 2:
-        while True:
-            print('-----------Borrar datos-----------')  
+            except ValueError:
+                print('Error de dato, vuelva a intentar')
+                continue
+        if continuaringreso == 1:
+            continue
+        else:
+            break
+def borrardatos():
+    while True:
+        print('-----------Borrar datos-----------') 
+        if not diccionariocalculos:
+            print('No existen operaciones guardadas')
+            break
+        else:
+            listardiccionario()
             llaveborrar = input ('Ingrese la operación a borrar:\n').capitalize()
-            borrardatos(llaveborrar)
+            if llaveborrar in diccionariocalculos:
+                del diccionariocalculos[llaveborrar]
+                print(f'Operación {llaveborrar} borrada exitosamente')
+            else:
+                print(f'No se ha encontrado la operación {llaveborrar}')
+                break
             while True:
                     try:
                         print('¿Desea borrar otra operación?')
@@ -73,17 +59,50 @@ while True:
                 continue
             else:
                 break
-    elif opcionmenu == 3:
-        print('-----------Listado de datos-----------')
-        if not diccionariocalculos:
-                print('No existen operatorias')
-        else:
-            for llave,valor in diccionariocalculos.items():
-                print(f'Operatoria:{llave}\nResultado:{valor}')        
-    elif opcionmenu == 4:
+def probarllave():
+    print('-----------Probar operaciones-----------')
+    listardiccionario()
+    if diccionariocalculos:
         llaveprueba = input("Ingrese la operatoria que desea probar:\n").capitalize()
-        probarllave(llaveprueba)
-    elif opcionmenu == 5:
-        print('Saliendo del programa, Gracias por su preferencia')
+        if llaveprueba in diccionariocalculos:
+            for llave, valor in diccionariocalculos.items():
+                if llaveprueba == llave:
+                    print(valor)
     else:
-       print('Opción inválida')
+        print('Volviendo al menu principal')
+def listardiccionario():
+    if not diccionariocalculos:
+        print('No existen operatorias guardadas')
+    else:
+        print ('Las operatorias disponibles son las siguientes:')
+        for llave, valor in diccionariocalculos.items():
+            print(f'Operatoria:{llave}\nResultado:{valor}')     
+while True:
+    try:
+        print ('''
+-----------Calculadora-----------
+1) Ingresar datos al diccionario
+2) Borrar datos del diccionario
+3) Listar los datos del diccionario
+4) Probar la operación
+5) Salir del programa
+    ''')
+        opcionmenu = int(input('Ingrese una opción:\n'))
+        if opcionmenu >5 or opcionmenu <1:
+            print('Opción inválida, vuelva a intentar')
+            continue
+    except ValueError:
+        print ('Error de datos, intente nuevamente')
+    match opcionmenu:
+        case 1:
+            ingresodatos()
+        case 2:
+            borrardatos()
+        case 3:
+            print('-----------Listado de datos-----------')
+            listardiccionario()
+        case 4:
+            probarllave()
+        case 5:
+            print('Saliendo del programa, Gracias por su preferencia')
+            break
